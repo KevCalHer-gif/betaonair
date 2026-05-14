5756e2f5bd9ccc41327babf70327c7fee0107962
Kevin Calle Herrera <herreracalle7672@gmail.com>
2026-05-13 23:32:11 -0400

    Actualizacion final del proyecto

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -147,6 +147,7 @@ export interface UserAuthOperations {
  */
 export interface User {
   id: number;
+  role: 'admin' | 'editor' | 'viewer';
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -487,6 +488,7 @@ export interface PayloadMigration {
  * via the `definition` "users_select".
  */
 export interface UsersSelect<T extends boolean = true> {
+  role?: T;
   updatedAt?: T;
   createdAt?: T;
   email?: T;
